16549597a3b009f3fa17b289af7bc1fcdb473bb7
Artidongare <artidongare22@gmail.com>
2022-05-23 18:53:04 +0530

    new1

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by web on 11/2/17.
- */
-
-var nodeServer = require('./node_server');
-var mongo = require('./db_connections/mongo_connection');
-nodeServer.server;
-mongo.mongoConnection();
-
